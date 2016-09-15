13900def085a5e2529f2a922adbd61ac815980eb
Tony Edgin <tedgin@iplantcollaborative.org>
2016-09-15 15:07:19 -0700

    switched to installer-gen in subtree

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -20,15 +20,15 @@ esac
 /src/packaging/build.sh clean
 /src/packaging/build.sh -r icommands
 /src/packaging/make_icommands_for_distribution.sh
-"$(dirname $0)"/gen-installer /packages/"$OS"/irods-icommands-4.1.9-"$OS".installer \
-                              /src/icommands.tgz \
-                              "$(dirname $0)"/config-icommands.sh
+"$(dirname $0)"/installer-gen/gen-installer /packages/"$OS"/irods-icommands-4.1.9-"$OS".installer \
+                                            /src/icommands.tgz \
+                                            "$(dirname $0)"/config-icommands.sh
 
 mkdir --parents /src/irods-fuse/bin /src/irods-fuse/lib
 cp /src/iRODS/clients/fuse/bin/irodsFs /src/irods-fuse/bin
 cp "$LibDir"/libfuse.so.2 /src/irods-fuse/lib
 tar --create --gzip --directory /src --file /src/irods-fuse.tgz irods-fuse
-"$(dirname $0)"/gen-installer /packages/"$OS"/irods-fuse-4.1.9-"$OS".installer \
-                              /src/irods-fuse.tgz \
-                              $(dirname "$0")/config-fuse.sh
+"$(dirname $0)"/installer-gen/gen-installer /packages/"$OS"/irods-fuse-4.1.9-"$OS".installer \
+                                            /src/irods-fuse.tgz \
+                                            $(dirname "$0")/config-fuse.sh
 rm --force --recursive /src/irods-fuse /src/irods-fuse.tgz
